ece121bfa22c481ec54240ffe1413c16e12fe42b
Ling Fung Chan <justinlfchan@gmail.com>
2022-10-29 00:05:23 +0100

    completed two_sum method

--- two_sum.rb
+++ two_sum.rb
@@ -3,7 +3,23 @@
 # @return {Integer[]}
 
 def two_sum(nums, target)
-  for i in 0..nums.length do
-
+  index = []
+  for i in 0...nums.length do
+    for j in (i + 1)...nums.length do
+      index = [i, j] if nums[i] + nums[j] == target
+    end
   end
+  index
 end
+
+nums = [2,7,11,15]
+target = 9
+p two_sum(nums, target)
+
+nums = [3,2,4]
+target = 6
+p two_sum(nums, target)
+
+nums = [3,3]
+target = 6
+p two_sum(nums, target)
